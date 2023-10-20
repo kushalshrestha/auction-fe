dc3dea6093bbe22b93199d0d87020cd10ffe1fed
kushalshrestha <kushalshr@gmail.com>
2023-10-20 15:14:30 -0500

    chore: constants added

---
+++ src/app/constants.js
@@ -0,0 +1,10 @@
+export const USER_ROLES = {
+    ADMIN: 'ADMIN',
+    SELLER: 'SELLER',
+    CUSTOMER: 'CUSTOMER',
+  };
+
+export const PROPERTY_STATUS = {
+    AVAILABLE: 'AVAILABLE', 
+    SOLD: 'SOLD', 
+};
