3c138c635b7db1f86efe1de7db5c0e199b3252f1
pratima81 <engpshah@gmail.com>
2023-10-23 20:40:24 -0500

    ongoing

--- src/components/product/ProductAddEdit.jsx
+++ src/components/product/ProductAddEdit.jsx
@@ -1,7 +1,61 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import axios from 'axios';
+import { httpPost, httpGet } from '../../api';
+import { Link } from 'react-router-dom';
+
+import { notifyError, notifySuccess } from './../../helpers/notification';
+
 function ProductAddEdit() {
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+    startingPrice: '',
+    deposit: '',
+    bidDueDate: '',
+    biddingPaymentDueDate: '',
+    released: false,
+  });
+
+  const navigate = useNavigate();
+
+  const handleProductInputChange = (e) => {
+    let { value } = e.target;
+    if (e.target.type === 'submit') {
+      console.log(value);
+      setFormData({
+        ...formData,
+        released: value === 'publish' ? true : false,
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [e.target.name]: value,
+      });
+    }
+  };
+
+  const handleProductSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    try {
+      const res = await httpPost({
+        url: '/products',
+        data: formData,
+      });
+
+      notifySuccess('product created successfully');
+
+      navigate('/products', { replace: true });
+    } catch (err) {
+      notifyError(`${err}`);
+    }
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleProductSubmit}>
         <div className="space-y-12">
           <div className="border-b border-gray-900/10 pb-1">
             <h2 className="text-base font-semibold leading-7 text-gray-900">Add Product</h2>
@@ -18,8 +72,11 @@ function ProductAddEdit() {
                 <div className="mt-2">
                   <input
                     type="text"
-                    name="product-name"
-                    id="product-name"
+                    name="name"
+                    id="name"
+                    required
+                    onChange={handleProductInputChange}
+                    value={formData.name}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -38,6 +95,9 @@ function ProductAddEdit() {
                     id="description"
                     name="description"
                     rows={3}
+                    required
+                    onChange={handleProductInputChange}
+                    value={formData.description}
                     className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                   <p className="mt-3 text-sm leading-6 text-gray-600">
@@ -58,8 +118,11 @@ function ProductAddEdit() {
                 <div className="mt-2">
                   <input
                     type="number"
-                    name="starting-price"
-                    id="starting-price"
+                    name="startingPrice"
+                    id="startingPrice"
+                    required
+                    onChange={handleProductInputChange}
+                    value={formData.startingPrice}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -77,6 +140,9 @@ function ProductAddEdit() {
                     type="number"
                     name="deposit"
                     id="deposit"
+                    required
+                    onChange={handleProductInputChange}
+                    value={formData.deposit}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -91,9 +157,12 @@ function ProductAddEdit() {
                 </label>
                 <div className="mt-2">
                   <input
-                    id="bid-due-date"
-                    name="bid-due-date"
+                    id="bidDueDate"
+                    name="bidDueDate"
                     type="text"
+                    required
+                    onChange={handleProductInputChange}
+                    value={formData.bidDueDate}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -107,9 +176,12 @@ function ProductAddEdit() {
                 </label>
                 <div className="mt-2">
                   <input
-                    id="bid-payment-due-date"
-                    name="bid-payment-due-date"
+                    id="biddingPaymentDueDate"
+                    name="biddingPaymentDueDate"
                     type="text"
+                    required
+                    onChange={handleProductInputChange}
+                    value={formData.biddingPaymentDueDate}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -120,24 +192,30 @@ function ProductAddEdit() {
 
         <div className="mt-6 flex items-center justify-end gap-x-6">
           <button
-            type="button"
+            type="submit"
+            value="publish"
+            onClick={handleProductInputChange}
             className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
           >
             Publish
           </button>
           <button
-            type="button"
+            value="draft"
+            type="submit"
+            onClick={handleProductInputChange}
             className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
           >
             Save as draft
           </button>
 
-          <button
-            type="button"
-            className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-          >
-            Cancel
-          </button>
+          <Link to="/products">
+            <button
+              type="button"
+              className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            >
+              Cancel
+            </button>
+          </Link>
         </div>
       </form>
     </>
